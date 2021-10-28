a77b11f85621d12b087977c27302dadf27ed9b39
Felipe Forbeck <felipe.forbeck@gmail.com>
2021-10-28 13:28:14 -0300

    feat: sign typed data with gcp kms (#2)
    * feat: sign typed data with kms
    
    * chore: change ts build
    
    * chore: change ts build
    
    * fix: using hex hash of typed data
    
    * feat: added recover typed sig

--- src/signer.ts
+++ src/signer.ts
@@ -1,9 +1,21 @@
 import configs from "dotenv";
+import {
+  MessageTypes,
+  SignTypedDataVersion,
+  TypedDataV1,
+  TypedMessage,
+  typedSignatureHash,
+  TypedDataUtils,
+} from "@metamask/eth-sig-util";
 
 import { ethers, UnsignedTransaction } from "ethers";
+import { bufferToHex } from "ethereumjs-util";
 import { getPublicKey, getEthereumAddress, requestKmsSignature, determineCorrectV } from "./util/gcp-kms-utils";
+import { validateVersion } from "./util/signature-utils";
 
 configs.config();
+
+export const TypedDataVersion = SignTypedDataVersion;
 
 export interface GcpKmsSignerCredentials {
   projectId: string;
@@ -46,6 +58,54 @@ export class GcpKmsSigner extends ethers.Signer {
 
   async signMessage(message: string | ethers.utils.Bytes): Promise<string> {
     return this._signDigest(ethers.utils.hashMessage(message));
+  }
+
+  /**
+   * Original implementation takes into account the private key, but here we use the private
+   * key from the GCP KMS, so we don't need to provide the PK as signature option.
+   * Source code: https://github.com/MetaMask/eth-sig-util/blob/main/src/sign-typed-data.ts#L510
+   * .
+   * Sign typed data according to EIP-712. The signing differs based upon the `version`.
+   *
+   * V1 is based upon [an early version of EIP-712](https://github.com/ethereum/EIPs/pull/712/commits/21abe254fe0452d8583d5b132b1d7be87c0439ca)
+   * that lacked some later security improvements, and should generally be neglected in favor of
+   * later versions.
+   *
+   * V3 is based on [EIP-712](https://eips.ethereum.org/EIPS/eip-712), except that arrays and
+   * recursive data structures are not supported.
+   *
+   * V4 is based on [EIP-712](https://eips.ethereum.org/EIPS/eip-712), and includes full support of
+   * arrays and recursive data structures.
+   *
+   * @param options - The signing options.
+   * @param options.data - The typed data to sign.
+   * @param options.version - The signing version to use.
+   * @returns The '0x'-prefixed hex encoded signature.
+   */
+  async signTypedData<V extends SignTypedDataVersion, T extends MessageTypes>({
+    data,
+    version,
+  }: {
+    data: V extends "V1" ? TypedDataV1 : TypedMessage<T>;
+    version: V;
+  }): Promise<string> {
+    validateVersion(version);
+
+    if (data === null || data === undefined) {
+      throw new Error("Missing data parameter");
+    }
+
+    let messageSignature: Promise<string>;
+    if (version === SignTypedDataVersion.V1) {
+      messageSignature = this._signDigest(typedSignatureHash(data as TypedDataV1));
+    } else {
+      const eip712Hash: Buffer = TypedDataUtils.eip712Hash(
+        data as TypedMessage<T>,
+        version as SignTypedDataVersion.V3 | SignTypedDataVersion.V4
+      );
+      messageSignature = this._signDigest(bufferToHex(eip712Hash));
+    }
+    return messageSignature;
   }
 
   async signTransaction(transaction: ethers.utils.Deferrable<ethers.providers.TransactionRequest>): Promise<string> {
